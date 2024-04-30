6d18f8da949a6f5f7b4d133b30281ffd5556f55f
Shubham Kumar Singh <112364851+ShubhamSingh120@users.noreply.github.com>
2024-04-30 06:18:12 +0000

    01, 02 files

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,8 @@
+const account_Id = 144552
+let account_Email = "singhshubham0051@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gurugram" // Possible
+
+// Prefer not to use var because of issue in block scope and functional scope
+
+console.table([account_Id, account_Email, accountPassword, accountCity])
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (empty value)
+// undefined => not defined value
+// symbol => unique
+
+console.log(typeof "shubham")
+
+// object
