4638e4e3dad16cf9ee62fc0becb3dbc37aec9153
Shubham Kumar Singh <112364851+ShubhamSingh120@users.noreply.github.com>
2024-04-30 09:51:25 +0000

    06

---
+++ 01_Basics/06_String.js
@@ -0,0 +1,30 @@
+const name = "Shubham"
+var age = 22
+
+// console.log(name + age)
+
+// console.log(`Hello, My name is ${name} and I am ${age} year old.`)
+
+const name2 = new String("ShubhamSingh")
+
+// console.log(name2.length)
+// console.log(name2.__proto__)
+
+// console.log(name2.toUpperCase())
+// console.log(name2.charAt(2))
+// console.log(name2.indexOf('g'))
+// console.log(name2)
+
+const newString = "     Shubham     "
+// console.log(newString)
+// console.log(newString.trim())
+
+const url = "http://singhshubham0051@gmail.com/shubham%20singh"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+const a = "My Name is Shubham Kumar Singh"
+
+console.log(a.split(' '))
