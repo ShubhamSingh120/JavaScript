f11dd837f9ea3aefcf77129f129afd3297f3562b
Shubham Kumar Singh <112364851+ShubhamSingh120@users.noreply.github.com>
2024-05-01 05:38:30 +0000

    07, 08

---
+++ 01_Basics/07_Numbers.js
@@ -0,0 +1,15 @@
+const num = 500
+console.log(num)
+
+const num2 = new Number(400)
+console.log(num2)
+
+console.log(num2.toString().length)
+console.log(num2.toFixed(2))
+
+const num3 = 231.7879
+console.log(num3.toPrecision(4))
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString())
+console.log(hundreds.toLocaleString('en-IN'))
